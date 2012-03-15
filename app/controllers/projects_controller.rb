9eddb755493c95096a8f7554577317cf534eaf35
Prakhar Srivastav <prakhar1989@gmail.com>
2012-03-16 00:18:44 +0530

    Corrected 404 on projects, implemented user redirection

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,7 @@
 class ProjectsController < ApplicationController
+
+  before_filter :find_project, :only => [:show, :edit, :destroy, :update]
+
   def index
     @projects = Project.all
   end
@@ -19,11 +22,11 @@ class ProjectsController < ApplicationController
   end
   
   def show
-    @project = Project.find(params[:id])
+    #filter doing this job
   end
 
   def edit
-    @project = Project.find(params[:id])
+    #filter doing this job
   end
 
   def update
@@ -46,4 +49,12 @@ class ProjectsController < ApplicationController
     redirect_to projects_path
   end  
 
+  private
+  
+    def find_project
+      @project = Project.find(params[:id])
+      rescue ActiveRecord::RecordNotFound
+      flash[:alert] = "The project you were looking for could not be found."
+      redirect_to projects_path
+    end
 end
--- spec/requests/project_pages_spec.rb
+++ spec/requests/project_pages_spec.rb
@@ -1,22 +1,4 @@
 require 'spec_helper'
 
 describe "ProjectPages" do
-
-  describe "index page" do
-
-    it "should have heading 'All Projects'" do
-      visit root_path
-      page.should have_selector('h2', 'Projects')
-    end
-
-  end
-
-  describe "new page" do
-
-    it "should have heading new project" do
-      visit '/projects/new'
-      page.should have_content('New Project')
-    end
-  end
-
 end
