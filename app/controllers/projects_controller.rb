7153dfbe44c36306f0fc991c815f1d6581faa778
Prakhar Srivastav <prakhar1989@gmail.com>
2012-03-15 23:14:07 +0530

    Added updating funcationality

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -22,4 +22,22 @@ class ProjectsController < ApplicationController
     @project = Project.find(params[:id])
   end
 
+  def edit
+    @project = Project.find(params[:id])
+  end
+
+  def update
+
+    @project = Project.find(params[:id])
+
+    if @project.update_attributes(params[:project])
+      flash[:success] = "Project has been updated"
+      redirect_to @project
+    else
+      flash[:alert] = "Project has not been updated"
+      render 'edit'
+    end
+  end
+  
+
 end
