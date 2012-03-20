1ce54ad3046ed42b4612bef0b330497b9d30de5e
Prakhar Srivastav <prakhar1989@gmail.com>
2012-03-21 05:19:16 +0530

    Added CRUD for assigning permissions interface

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,13 @@ Tickteee::Application.routes.draw do
 
   namespace :admin do
     root :to => "base#index"
-    resources :users
+    resources :users do
+      resources :permissions
+    end
   end
+
+  put '/admin/users/:user_id/permissions',
+    :to => "admin/permissions#update",
+    :as => :update_user_permissions
 
 end
