21c7fce3b472bf192cba20b9e48aeca514160e00
Prakhar Srivastav <prakhar1989@gmail.com>
2012-03-26 22:35:37 +0530

    Added display of state transition

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,5 @@
 class Comment < ActiveRecord::Base
+  before_create :set_previous_state
   after_create :set_ticket_state
 
   validates_presence_of :text
@@ -6,6 +7,7 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :state
   delegate :project, :to => :ticket
+  belongs_to :previous_state, :class_name => "State"
 
   private
 
@@ -13,5 +15,10 @@ class Comment < ActiveRecord::Base
       self.ticket.state = self.state
       self.ticket.save!
     end
+
+    def set_previous_state
+      self.previous_state = ticket.state
+    end
 end
+
 
