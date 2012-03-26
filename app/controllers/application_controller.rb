d15c44db9e62df308caa0f032150e8ca1c9fa44a
Prakhar Srivastav <prakhar1989@gmail.com>
2012-03-26 23:17:53 +0530

    Add ability to create new states by admins

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :find_states
 
   def authorize_admin!
     authenticate_user!
@@ -7,6 +8,10 @@ class ApplicationController < ActionController::Base
       flash[:alert] = "You must be an admin to do that."
       redirect_to root_path
     end
+  end
+
+  def find_states
+    @states = State.all
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Tickteee::Application.routes.draw do
 
   namespace :admin do
     root :to => "base#index"
+    resources :states
     resources :users do
       resources :permissions
     end
