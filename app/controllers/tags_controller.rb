3cc73fff3be6994fe532af209da07ff034f5c635
Prakhar Srivastav <prakhar1989@gmail.com>
2012-03-28 02:17:18 +0530

    Added remove tag functionality

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,5 +1,10 @@
-class TagsController < ApplicationController
+class TagsController < ApplicationController  
   def remove
-   @ticket = Ticket.find(params[:ticket_id]) 
+    @ticket = Ticket.find(params[:ticket_id])
+    if can?(:tag, @ticket.project) || current_user.admin?
+      @tag = Tag.find(params[:id])
+      @ticket.tags -= [@tag]
+      @ticket.save
+    end
   end
 end
