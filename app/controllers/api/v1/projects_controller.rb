f709f1daa44248e215f6c92aad8d73f8fbb1d252
Prakhar Srivastav <prakhar1989@gmail.com>
2012-03-30 03:52:36 +0530

    Restricted admins to create projects through API

--- app/controllers/api/v1/projects_controller.rb
+++ app/controllers/api/v1/projects_controller.rb
@@ -1,5 +1,18 @@
 class Api::V1::ProjectsController < Api::V1::BaseController 
+  before_fiter :authorize_admin!, :except => [:index, :show]
+
   def index
     respond_with(Project.for(current_user).all)
   end
+  
+  def create
+    project = Project.create(params[:project])
+    if project.valid?
+      respond_with(project, :location => api_v1_project_path(project))    
+    else
+      respond_with(project)
+    end
+  end
+
+
 end
